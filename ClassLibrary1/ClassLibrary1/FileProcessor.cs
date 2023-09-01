a3500b18be249c7b80a797c72edd0dcc2a69755e
GeorgiKadiev <georgi.k.kadiev@gmail.com>
2023-09-01 22:52:56 +0300

    add class library

---
+++ ClassLibrary1/ClassLibrary1/FileProcessor.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ClassLibrary
+{
+    public class FileProcessor
+    {
+
+        public static string ReadFromFile()
+        {
+            string fileName = "DataEntry.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Data\JSON\", fileName);
+            string sFilePath = Path.GetFullPath(path);
+
+            using (var streamReader = new StreamReader(sFilePath))
+                return streamReader.ReadToEnd();
+        }
+
+        public static void SaveToFile(string data)
+        {
+            string fileName = "DataEntry.json";
+            string filePath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Data\JSON\");
+            string filePathh = Path.Combine(filePath, fileName);
+
+            if (File.Exists(filePathh))
+            {
+                File.Delete(filePathh);
+            }
+
+            string fullPath = Path.Combine(filePath, fileName);
+
+            File.WriteAllText(fullPath, data);
+        }
+
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/Validator.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassLibrary
+{
+    public class Validator
+    {
+        public static bool IsNumber(List<string> list)
+        {
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!double.TryParse(list[i], out _))
+                    return false;
+
+
+            }
+            return true;
+        }
+
+        public static bool IsZero(List<double> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool IsDiagonalLongest(List<double> list)
+        {
+            if (list[1] >= list[0] * 2)
+                return false;
+
+            return true;
+        }
+    }
+}
